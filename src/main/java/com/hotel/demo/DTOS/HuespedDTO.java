422e921086af35cde8abfefb755594b580159430
JenniferCamones <jenifercamonesquiroz@gmail.com>
2024-10-22 12:13:10 -0500

    kj
    jh

--- src/main/java/com/hotel/demo/DTOS/HuespedDTO.java
+++ src/main/java/com/hotel/demo/DTOS/HuespedDTO.java
@@ -1,6 +1,7 @@
 package com.hotel.demo.DTOS;
 
 import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,18 +15,23 @@ public class HuespedDTO {
 	private int id_huesped;
 
 	@NotNull(message = "Campo Nombre Huesped no puede ser nulo")
+	@NotBlank(message = "Campo Nombre Huesped no puede ser nulo")
 	private String nombre;
 
 	@NotNull(message = "Campo Apellido Huesped no puede ser nulo")
+	@NotBlank(message = "Campo Apellido Huesped no puede ser nulo")
 	private String apellido;
 
 	@NotNull(message = "Campo Direccion Huesped no puede ser nulo")
+	@NotBlank(message = "Campo Direccion Huesped no puede ser nulo")
 	private String direccion;
 
 	@NotNull(message = "Campo Email Huesped no puede ser nulo")
+	@NotBlank(message = "Campo Email Huesped no puede ser nulo")
 	private String email;
 
 	@NotNull(message = "Campo Sexo Huesped no puede ser nulo")
+	@NotBlank(message = "Campo Sexo Huesped no puede ser nulo")
 	private String sexo;
 
 	@NotNull(message = "Campo Edad Huesped no puede ser nulo")
